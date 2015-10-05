3456108235694b1694b139a9cfa9c2d1fd88c6a0
raikkon <raikkonlin@gmail.com>
2015-10-05 14:40:25 +0800

    Initial Commit

---
+++ ElloDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ElloDemo
+//
+//  Created by LINCHUNGYAO on 2015/10/5.
+//  Copyright © 2015年 LINCHUNGYAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ElloDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ElloDemo
+//
+//  Created by LINCHUNGYAO on 2015/10/5.
+//  Copyright © 2015年 LINCHUNGYAO. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
